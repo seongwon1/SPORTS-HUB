d56408b17bd9e02fcb8d4e713a25d53f4746ce01
seongwon1 <tlrhdtk123@naver.com>
2022-07-21 01:03:54 +0900

    0720일자 커밋, 커밋시간:7월21일 오전 1:03

--- 효근/js/index.js
+++ 효근/js/index.js
@@ -1,20 +1,16 @@
-let btn= document.querySelectorAll(".section1-top-imgbox-hover_select_img");
+let btn= document.querySelectorAll(".section1-top-imgbox-hover_select_color");
 let change= document.querySelector(".section1-top-imgbox-img");
 
     btn[0].addEventListener('mouseover',function(){
-        change.src= "/인경/img/여자/나이키 브라탑/나이키 브라탑  검정.png";
+        change.src= "img/여자/나이키 브라탑/나이키 브라탑  검정.png";
     })
     btn[1].addEventListener('mouseover',function(){
-        change.src= "/인경/img/여자/나이키 브라탑/나이키 브라탑 보라색.png";
+        change.src= "img/여자/나이키 브라탑/나이키 브라탑 보라색.png";
     })
     btn[2].addEventListener('mouseover',function(){
-        change.src= "/인경/img/여자/나이키 브라탑/나이키 브라탑 분홍.png";
+        change.src= "img/여자/나이키 브라탑/나이키 브라탑 분홍.png";
     })
     btn[3].addEventListener('mouseover',function(){
-        change.src= "/인경/img/여자/나이키 브라탑/나이키 브라탑 화이트.png";
+        change.src= "img/여자/나이키 브라탑/나이키 브라탑 화이트.png";
     })
-
-
-
-
 
